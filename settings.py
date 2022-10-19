512808d98f28f8e581b13614f51ef6716e040e82
Samu Reinikainen <samu.reinikainen@gmail.com>
2022-10-19 19:32:46 +0300

    fixed missing comma

--- settings.py
+++ settings.py
@@ -8,6 +8,6 @@
 htmlSettings = {
     "*LANG*": "en",
     "*TITLE*": "Samu Reinikainen CV",
-    "*DESC*": "Tiny CV app made with Python and served with Flask"
+    "*DESC*": "Tiny CV app made with Python and served with Flask",
     "*AUTH*": "samu.reinikainen@gmail.com"
 }
