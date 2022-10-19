d854d25250773349fd1eff04f174b5594d6ad4a9
Samu Reinikainen <samu.reinikainen@gmail.com>
2022-10-19 18:46:14 +0300

    created settings.py

---
+++ settings.py
@@ -0,0 +1,6 @@
+htmlSettings = {
+    "*LANG*": "en",
+    "*TITLE*": "Samu Reinikainen CV",
+    "*DESC*": "Tiny CV app made with Python and served with Flask"
+    "*AUTH*": "samu.reinikainen@gmail.com"
+}
