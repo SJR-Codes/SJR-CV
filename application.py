9185832c937c78dc34f84dcf6e0eb3227ba91774
Samu Reinikainen <samu.reinikainen@gmail.com>
2022-10-19 19:12:50 +0300

    drafting in progress

--- application.py
+++ application.py
@@ -4,10 +4,36 @@
 * Flask application for CV web page
 * Created by Samu Reinikainen
 """
-
 from flask import Flask
-app = Flask(__name__)
+import settings
 
-@app.route("/")
-def hello():
-    return "<html><body><h1>My CV</h1></body></html>\n"
+class CV:
+    def __init__(self, template = "template.html", style="style.css"):
+        self.htmlTemplate = fetchFile(template)
+        self.style = fetchFile(style)
+
+def main():
+    #initialize flask app
+    app = Flask(__name__)
+
+    #initialize CV
+    #override defaults: cv = CV(template="*yourtemplate.html*", style="*yourstyle.css*")
+    #cv = CV()
+
+    cv = "<h1>One moment, please...</h1>"
+
+    #respond all queries with CV page
+    @app.route("/")
+    def hello():
+        return cv
+
+def fetchFile(filen: str) -> str:
+    try:
+        with open(filen, "r") as file:
+            return file.read()
+    except FileNotFoundError:
+        exit("Error: script file not found")
+
+# run blocks of code only if our program is the main program executed
+if __name__ == "__main__":
+    main()
