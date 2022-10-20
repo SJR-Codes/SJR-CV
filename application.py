a431276408b7d2bdff2954e3f9ac3fa244635d23
Samu Reinikainen <samu.reinikainen@gmail.com>
2022-10-20 10:49:34 +0300

    refactored setHtmlSettings to class method

--- application.py
+++ application.py
@@ -10,24 +10,36 @@ import funcs as f
 
 class CV:
     def __init__(self, template = "template.html", style="style.css"):
-        self.htmlTemplate = f.fetchFile(template)
+        self.html = f.fetchFile(template)
         self.style = f.fetchFile(style)
         #set inline styles to template
-        self.htmlTemplate = self.htmlTemplate.replace("*STYLE*", self.style)
-        self.htmlTemplate = f.setHtmlSettings(self.htmlTemplate, sets.htmlSettings)
+        self.html = self.html.replace("*STYLE*", self.style)
         
-    def addBody(self, content:str) -> None:        
+    def setHtmlSettings(self, settings: dict) -> None:
+        """
+        Set htmlSettings values to html placeholders.
+        :param html: html source as string
+        :type html: str
+        :param settings: settings as dict
+        :type settings: dict
+        :return: string
+        :rtype: str
+        """
+        for key, val in settings.items():
+            self.html = self.html.replace(key, val)
+        
+    def addBody(self, content:str) -> None:
         """
         Insert content to html body.
         :param content: html content
         :type content: str
         :return: None
         """
-        self.htmlTemplate = self.htmlTemplate.replace("*BODY*", content)
+        self.html = self.html.replace("*BODY*", content)
 
     #define what class instance prints when print() or str()
     def __str__(self):
-         return self.htmlTemplate
+         return self.html
 
 #initialize flask app
 app = Flask(__name__)
@@ -35,6 +47,7 @@ app = Flask(__name__)
 #initialize CV
 #override defaults: cv = CV(template="*yourtemplate.html*", style="*yourstyle.css*")
 cv = CV()
+cv.setHtmlSettings(sets.htmlSettings)
 cv.addBody("<h1>CV 20.10.2022</h1>")
 
 #respond all queries with CV page
