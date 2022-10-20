07b7ee94633354f047f3b7e5d8200722aacba9e7
Samu Reinikainen <samu.reinikainen@gmail.com>
2022-10-20 10:37:28 +0300

    drafting continues

--- application.py
+++ application.py
@@ -5,34 +5,43 @@
 * Created by Samu Reinikainen
 """
 from flask import Flask
-import settings
+import settings as sets
+import funcs as f
 
 class CV:
     def __init__(self, template = "template.html", style="style.css"):
-        self.htmlTemplate = fetchFile(template)
-        self.style = fetchFile(style)
+        self.htmlTemplate = f.fetchFile(template)
+        self.style = f.fetchFile(style)
+        #set inline styles to template
+        self.htmlTemplate = self.htmlTemplate.replace("*STYLE*", self.style)
+        self.htmlTemplate = f.setHtmlSettings(self.htmlTemplate, sets.htmlSettings)
+        
+    def addBody(self, content:str) -> None:        
+        """
+        Insert content to html body.
+        :param content: html content
+        :type content: str
+        :return: None
+        """
+        self.htmlTemplate = self.htmlTemplate.replace("*BODY*", content)
 
+    #define what class instance prints when print() or str()
+    def __str__(self):
+         return self.htmlTemplate
 
 #initialize flask app
 app = Flask(__name__)
 
 #initialize CV
 #override defaults: cv = CV(template="*yourtemplate.html*", style="*yourstyle.css*")
-#cv = CV()
-
-cv = "<h1>Dos momentos, por favor...</h1>"
+cv = CV()
+cv.addBody("<h1>CV 20.10.2022</h1>")
 
 #respond all queries with CV page
 @app.route("/")
 def hello():
-    return cv
+    return str(cv)
 
-def fetchFile(filen: str) -> str:
-    try:
-        with open(filen, "r") as file:
-            return file.read()
-    except FileNotFoundError:
-        exit("Error: script file not found")
 
 # run blocks of code only if our program is the main program executed
 if __name__ == "__main__":
