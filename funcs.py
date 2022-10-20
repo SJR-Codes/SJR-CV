76265c601004317012568f9e6364d9337ab5550b
Samu Reinikainen <samu.reinikainen@gmail.com>
2022-10-20 10:48:05 +0300

    moved setHtmlSettings to class

--- funcs.py
+++ funcs.py
@@ -18,20 +18,3 @@ def fetchFile(filen: str) -> str:
             return file.read()
     except FileNotFoundError:
         exit("Error: script file not found")
-
-
-def setHtmlSettings(html: str, settings: dict) -> str:
-    """
-    Set htmlSettings values to html placeholders.
-    :param html: html source as string
-    :type html: str
-    :param settings: settings as dict
-    :type settings: dict
-    :return: string
-    :rtype: str
-    """
-
-    for key, val in settings.items():
-        html = html.replace(key, val)
-
-    return html
