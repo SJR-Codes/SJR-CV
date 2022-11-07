33bbed8b121062197eaa09bf52dd554b8cb982a2
Samu Reinikainen <samu.reinikainen@gmail.com>
2022-11-07 13:47:37 +0200

    comments

--- application.py
+++ application.py
@@ -15,10 +15,12 @@ app = Flask(__name__)
 #initialize CV
 cv = CV()
 
+#add html blocks
 cv.addBlock("tmpl/head.html", "_htmlHead")
 cv.addBlock("tmpl/about.html", "_htmlAbout")
 cv.addMultiBlock("tmpl/block.html", "tmpl/experience.html", "_htmlExperience")
 
+#putting it all together
 cv.addBody()
 
 #respond all queries with CV page
